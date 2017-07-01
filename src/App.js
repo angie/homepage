e3fc05fe25835d703171559a59add8a056e09ed9
Angie Merryweather <hi@angie.io>
2017-07-01 16:03:36 +0100

    First commit 🎉

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="App-header">angie.io</div>
+    <h2>Hi, I'm Angie Merryweather</h2>
+    <h2 className="blue-accent">This is a blue accent.</h2>
+    <h2 className="yellow-accent">This is a yellow accent.</h2>
+    A full-stack software engineer from Manchester, UK.
+  </div>
+);
+
+export default App;
